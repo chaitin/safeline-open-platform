e84e2845e8fd0efe6f1c0edb30236148b4575dd7
monstersb <me@changzhi.li>
2019-01-02 11:42:32 +0800

    add threshold to risklevel

--- series_10/plugin_demo/半小时内发现三次攻击就封IP一小时/plugin.lua
+++ series_10/plugin_demo/半小时内发现三次攻击就封IP一小时/plugin.lua
@@ -8,42 +8,50 @@ match = {
     target  = skynet.MATCH_TARGET_DETECT,
 }
 
+function clear(ip)
+    skynet.db_del(skynet.DB_LOCAL, ip..":ts")
+    skynet.db_del(skynet.DB_LOCAL, ip..":attack")
+end
+
+function reset(log, ip)
+    skynet.db_set(skynet.DB_LOCAL, ip..":ts", tostring(log.timestamp))
+    skynet.db_add(skynet.DB_LOCAL, ip..":attack", 1)
+end
+
 function process(ip, host, urlpath)
-    local attack_limit = 2
-    local check_dur = 60*30
-    local ban_dur = 60*60
+    local attack_limit = 3     -- 累积攻击次数
+    local check_dur = 60 * 30  -- 攻击次数统计时间，单位秒
+    local ban_dur = 60 * 60    -- 封禁时间，单位秒
+    local ban_risk = 3         -- 需要统计的威胁等级，3 代表只统计高危，2 代表统计中高危，1代表统计中高低危
+
+    local log = skynet.get_detailed_info()
+
+    if log.risk_level < ban_risk then
+        return
+    end
 
     local ts = skynet.db_get(skynet.DB_LOCAL, ip..":ts")
-    local log = skynet.get_detailed_info()
-    local clear = function()
-        skynet.db_del(skynet.DB_LOCAL, ip..":ts")
-        skynet.db_del(skynet.DB_LOCAL, ip..":attack")
-    end
-    local reset = function()
-        skynet.db_set(skynet.DB_LOCAL, ip..":ts", tostring(log.timestamp))
-        skynet.db_add(skynet.DB_LOCAL, ip..":attack", 1)
-    end
 
     if ts == nil then
         -- first time record
-        reset()
+        reset(log, ip)
     else
         if log.timestamp - tonumber(ts) < check_dur then
             local attack = skynet.db_get(skynet.DB_LOCAL, ip..":attack")
-            if attack ~= nil and tonumber(attack) > attack_limit then
+            if attack ~= nil and tonumber(attack) >= attack_limit then
                 -- exceed attack limit
                 local key = {ip  = ip}
                 skynet.action_ban(key, ban_dur)
                 skynet.log("process", "ban "..ip)
-                clear()
+                clear(ip)
             else
                 -- normal record
                 skynet.db_add(skynet.DB_LOCAL, ip..":attack", 1)
             end
         else
             -- exceed check duration, reset record
-            clear()
-            reset()
+            clear(ip)
+            reset(log, ip)
         end
     end
 end
